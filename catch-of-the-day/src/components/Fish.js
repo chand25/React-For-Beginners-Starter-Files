67b2363bdb43f2a910c2a4941f231024d8086c6b
Shantel Moore <moore.shantel@gmail.com>
2017-01-02 21:43:17 -0500

    video completion

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
   render() {
-    <li className="menu-fish">pic</li>
+    const { details } = this.props;
+    // ES6 above or you can use const details = this.props.details;
+    return(
+       <li className="menu-fish">
+            <img src={details.image} alt={details.name} />
+          {/*if you are setting src  or alt to a variable the quotes are not needed*/}
+            <h3 className="fish-name">
+                {details.name}
+                <span className="price">{formatPrice(details.price)}</span>
+            </h3>
+            <p>{details.desc}</p>
+            <button>Add To Order</button>
+       </li>
+    )
   }
 }
 
