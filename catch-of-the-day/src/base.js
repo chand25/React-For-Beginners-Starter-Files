49e2db567ce969a2cb33b669a5aea23682673228
Shantel Moore <moore.shantel@gmail.com>
2017-01-09 11:13:31 -0500

    cause setup with dbase

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -3,7 +3,7 @@ import Rebase from 're-base';
 
 //needs api key, domain, database url
 const base = Rebase.createClass({
-    apiKey: "AIzaSyCYCDLYf7X2S-BkbDUCGMof1_9NRxxYKSE",
+    apiKey: ,
     authDomain: "catch-of-the-day-chand.firebaseapp.com",
     databaseURL: "https://catch-of-the-day-chand.firebaseio.com",
 });
